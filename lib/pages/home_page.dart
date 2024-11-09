1e6bff34883024d793d5bc5e43b6c57b45b39434
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-11-09 12:36:56 -0500

    mejorando el codigo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,35 +10,80 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  PersonModel personaModel1 = PersonModel(
-    name: "Mathias",
-    lastname: "Parra",
-    address: "Av 123",
-  );
-
-  List<int> numeros = [1, 2, 3];
-
-  List<Map<String, dynamic>> listaMapas = [
-    {
-      "mapa1clave": "valor1",
-    },
-    {
-      "mapa1clave2": "valor2",
-    },
-  ];
-
-  Map<String, dynamic> mapa1 = {
-    "clave": "valor",
-    "clave1": 123,
-    "list1": [1, 2, 3],
-    "mapa1": {"clave": "valor"},
-  };
-
   CarreraModel carrera1 = CarreraModel("Ing. Sistemas", "5 años");
 
   List<InstitutionModel> institutionList = [];
 
   List<MatriculaModel> matriculasList = [];
+
+  Widget cabeceraInstitucionWidget(InstitutionModel institution) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(institution.nombre),
+        IconButton(
+          onPressed: () {
+            institution.matriculasList.add(
+              MatriculaModel(
+                date: "09/11/2024",
+                hour: "11:26",
+                student: PersonModel(
+                    name: "Lucas", lastname: "Perez", address: "address"),
+                carrera: carrera1,
+              ),
+            );
+            setState(() {});
+          },
+          icon: Icon(Icons.add),
+        ),
+        IconButton(
+          onPressed: () {
+            institution.matriculasList.clear();
+            setState(() {});
+          },
+          icon: Icon(Icons.cleaning_services),
+        ),
+        IconButton(
+            onPressed: () {
+              institutionList.remove(institution);
+              setState(() {});
+            },
+            icon: Icon(Icons.delete))
+      ],
+    );
+  }
+
+  Widget buildMatriculaListTile(
+      InstitutionModel institution, MatriculaModel e) {
+    return ListTile(
+      title:
+          Text("${e.student.name} ${e.student.lastname} - ${e.carrera.nombre}"),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          IconButton(
+            color: Colors.red,
+            onPressed: () {
+              institution.matriculasList.remove(e);
+              setState(() {});
+            },
+            icon: Icon(Icons.delete),
+          ),
+          IconButton(
+            onPressed: () {
+              e.student = PersonModel(
+                  name: "Anita", lastname: "Duarte", address: "asdasdasd");
+              setState(() {});
+            },
+            icon: Icon(
+              Icons.edit,
+            ),
+            color: Colors.blue,
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,71 +117,13 @@ class _HomePageState extends State<HomePage> {
             ...institutionList.map(
               (institution) => Column(
                 children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(institution.nombre),
-                      IconButton(
-                        onPressed: () {
-                          institution.matriculasList.add(
-                            MatriculaModel(
-                              date: "09/11/2024",
-                              hour: "11:26",
-                              student: PersonModel(
-                                  name: "Lucas",
-                                  lastname: "Perez",
-                                  address: "address"),
-                              carrera: carrera1,
-                            ),
-                          );
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.add),
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          institution.matriculasList.clear();
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.cleaning_services),
-                      ),
-                    ],
-                  ),
+                  cabeceraInstitucionWidget(institution),
                   ...institution.matriculasList.map(
-                    (e) => ListTile(
-                      title: Text(
-                          "${e.student.name} ${e.student.lastname} - ${e.carrera.nombre}"),
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            color: Colors.red,
-                            onPressed: () {
-                              institution.matriculasList.remove(e);
-                              setState(() {});
-                            },
-                            icon: Icon(Icons.delete),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit,
-                            ),
-                            color: Colors.blue,
-                          )
-                        ],
-                      ),
-                    ),
-                  )
+                    (e) => buildMatriculaListTile(institution, e),
+                  ),
                 ],
               ),
             ),
-            // ...matriculasList.map(
-            //   (e) => ListTile(
-            //     title: Text(
-            //         "${e.student.name} ${e.student.lastname} - ${e.carrera.nombre}"),
-            //   ),
-            // )
           ],
         ),
       ),
